a2a135c81d7784053470860214122cd35b6a2db1
vedanthp16 <pvedanth2@gmail.com>
2022-01-03 20:56:56 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const functions = require("firebase-functions");
+const corse = require('cors')({origin: true});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+/* -exports.sendMessage = functions.firestore
+    .document('products/{productId}/qwertyu/{id}')
+    .onCreate((change,context) => {
+
+     const docId = context.params.productId;
+     const Id = context.params.id;
+
+     const name = change.data().name;
+     const productRef = admin.firestore().collection('products').doc(docId)
+
+     return productRef.update({ message: `Nice ${name}! = Love Cloud Functions`})
+ });*/
+
+ /*exports.Uploadfiile = functions.https
+       .onRequest((req, res) => { 
+           res.status(200).json({
+
+            message : 'It worked'
+           })
+          
+
+       })*/
+      // const db = firebase.firestore()
+       exports.SendDoc=functions.https.onRequest((req, res) =>{
+                           const prod = admin.firestore().collection('products');
+                           prod.get() 
+                                .then(e=>{
+                                    e.forEach(doc=>{
+                                        const dt = doc.data();
+                                         console.log(dt) 
+                                         res.json({
+                                             
+                                             object1: dt
+                                         });
+                                   });
+                                })
+                       })
